491e88f9a92c24693d990d28bb7f01456a1144c6
Jakub Jabłoński <jablonski.jak@gmail.com>
2017-05-06 17:26:50 +0200

    Comment Associated with User

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,21 +1,25 @@
 class CommentsController < ApplicationController
 
-
-  http_basic_authenticate_with name: "dhh", password: "secret", only: :destroy
-  
   def create
     @article = Article.find(params[:article_id])
     @comment = @article.comments.create(comment_params)
-    redirect_to article_path(@article)
+    @comment.user_id=current_user.id if current_user
+    @comment.save
+    if @comment.save
+      redirect_to article_path(@article)
+  else
+    render 'new'
   end
- 
+end
+
+
   def destroy
     @article = Article.find(params[:article_id])
     @comment = @article.comments.find(params[:id])
     @comment.destroy
     redirect_to article_path(@article)
   end
- 
+
   private
     def comment_params
       params.require(:comment).permit(:commenter, :body)
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170505184300) do
+ActiveRecord::Schema.define(version: 20170505190404) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
@@ -27,7 +27,9 @@ ActiveRecord::Schema.define(version: 20170505184300) do
     t.integer  "article_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
     t.index ["article_id"], name: "index_comments_on_article_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
