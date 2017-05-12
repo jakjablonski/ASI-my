3fc8dc7b9eb3fb0bee280434972a2d7c1c1096cb
Jakub Jabłoński <jablonski.jak@gmail.com>
2017-05-12 21:29:04 +0200

    fix up vote

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,8 @@ Rails.application.routes.draw do
   devise_for :users
   resources :articles do
     member do
-        get "like", to: "articles#upvote"
-        get "dislike", to: "articles#downvote"
+        put "like", to: "articles#upvote"
+        put "dislike", to: "articles#downvote"
     end
   resources :comments
 end
